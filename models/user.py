ca4a6624cfa8964b5c7bdd3845b5fbb7bc64b4dc
tambv6164 <44257256+tambv6164@users.noreply.github.com>
2018-12-14 00:08:35 +0700

    Create user.py

---
+++ models/user.py
@@ -0,0 +1,13 @@
+from mongoengine import Document, StringField, IntField, DateTimeField
+
+class User(Document):
+    fullname = StringField() # Tên đầy đủ
+    username = StringField() 
+    password = StringField()
+    email = StringField()
+    finished_arts = IntField(default=0)
+    working_arts = IntField(default=0)
+    totallikes = IntField(default=0)
+    picsintop100 = IntField(default=0)
+    positionintop100 = IntField(default=0)
+    # joineddate = DateTimeField() # Ngày tham gia trang web
