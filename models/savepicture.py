f9e44b640d19b286f46ee07497fe536187c3f3ad
tambv6164 <44257256+tambv6164@users.noreply.github.com>
2018-12-17 20:48:48 +0700

    Update savepicture.py

--- models/savepicture.py
+++ models/savepicture.py
@@ -6,5 +6,6 @@ class Savepicture(Document):
     picstatus = StringField(default='working') # 'working' là đang vẽ dở, mình sẽ update thành 'finished' là đã tô xong
     piclikes = IntField(defalt=0) # số like, ban đầu mặc định là 0, tăng lên mỗi khi có ngươi like
     picartist = StringField(default=None) # tranh này tác giả là ai, ban đầu mặc định là None. Nếu người dùng click vào save thì sẽ tính là tranh của người đó
+    picpositionintop100 = IntField(default=0) # ban đầu chưa được xếp hạng (chưa có like)
     # starttime = DateTimeField() # Ngày bắt đầu tô
     # finishtime = DateTimeField() # Ngày hoàn thành
